152324a5513af77e000bfe406bbc810ab17afb37
harrymknight <harry.knight@hotmail.co.uk>
2021-04-02 17:06:13 +0100

    Delete src directory

--- src/domain/Theme/DimensionSelectorContext/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const DimensionSelectorContext = React.createContext({
-    rows: 6, columns: 7
-});
--- src/hooks/useComponentId/index.js
+++
@@ -1,9 +0,0 @@
-import { useRef } from 'react';
-
-let uniqueId = 0;
-const getUniqueId = () => uniqueId++;
-
-export default function useComponentId() {
-  const idRef = useRef(getUniqueId());
-  return idRef.current;
-}
